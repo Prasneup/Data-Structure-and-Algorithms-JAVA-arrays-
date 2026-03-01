0381fd4613867f3a24e6160f5b6201b312481d75
Prasneup <prasannaneupane723@gmail.com>
2026-03-01 23:07:39 +0545

    Add transpose of matrix implementation

---
+++ src/arrays_2d/TransposeOfMatrix.java
@@ -0,0 +1,53 @@
+package src.arrays_2d;
+
+import java.util.*;
+public class TransposeOfMatrix {
+    static void printMatrix(int[][]matrix){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[i].length; j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[][] findTranspose(int[][] matrix, int r, int c){
+        int [][] ans = new int[c][r];
+        for(int i=0; i<c; i++){
+         for(int j=0; j<r; j++){
+            ans[i][j]=matrix[j][i];
+         }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows and columns of matrix");
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int[][] matrix = new int[r][c];
+        int totalElements = r*c;
+
+        System.out.println("Enter"+totalElements+" values");
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Input Matrix");
+        printMatrix(matrix);
+
+        System.out.println("Transpose of matrix");
+        int[][] transpose = findTranspose(matrix, r, c);
+        printMatrix(transpose);
+
+
+
+
+    }
+    
+}
